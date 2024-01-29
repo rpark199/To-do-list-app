d54ce66d7dde37255684f4fe7a97d9158ac08079
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-01-29 14:03:01 -0500

    JQuery addition and fixed error

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,32 @@
 function newItem(){
 
     //1. Adding a new item to the list of items: 
-       let li = $('<liv></liv>');
-       let inputValue = $('#input').val();
+       let li = $("<liv></liv>");
+       let inputValue = $("#input").val();
        li.append(inputValue);
     
-       if (inputValue === '') {
+       if ($("#input").val() === "") {
          alert("You must write something!");
        } else {
-         $('#list').append(li);
+         let list = $('#list');
+         list.append(li);
        }
     
      //2. Crossing out an item from the list of items:
-       function crossOut() {
-             li.toggleClass("strike");
-         }
-    
-         li.on("dblclick", function crossOut() {
+         li.on("dblclick", function () {
             li.toggleClass("strike");
          });
     
      //3(i). Adding the delete button "X": 
        let crossOutButton = $('<crossOutButton></crossOutButton>');
-         crossOutButton.append(document.createTextNode("X"));
+         $(crossOutButton).apprend("X");
          li.append(crossOutButton);
     
-         crossOutButton.on("click", deleteListItem);
      //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-       function deleteListItem(){
-             li.addClass("delete")
-         }
+       crossOutButton.on("click", function () {
+        li.addClass("delete");
+       });
+
      // 4. Reordering the items: 
        $('#list').sortable();
     }
